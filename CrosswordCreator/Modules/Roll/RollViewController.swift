02f7f76e970920d224e7bde4cdbf9cfbcd848eae
ads2019tasks <ads2019tasks@gmail.com>
2019-12-09 15:15:49 +0300

    questions search improvement

--- CrosswordCreator/Modules/Roll/RollViewController.swift
+++ CrosswordCreator/Modules/Roll/RollViewController.swift
@@ -75,18 +75,11 @@ final class RollViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        tableView.dataSource = self
-        tableView.delegate = self
-        
-        tableView.estimatedRowHeight = 44.0
-        tableView.rowHeight = UITableView.automaticDimension
-        
-        tableView.register(UINib(nibName: "\(RollCell.self)", bundle: Bundle.main),
-                           forCellReuseIdentifier: cellIdentifier)
-        
         title = mode == .questions ? Constants.questions : Constants.words
         
+        setupTableView()
         setupSearchController()
+        setupKeyboard()
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -110,6 +103,17 @@ final class RollViewController: UIViewController {
     
     // MARK: Private
     
+    private func setupTableView() {
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        tableView.estimatedRowHeight = 44.0
+        tableView.rowHeight = UITableView.automaticDimension
+        
+        tableView.register(UINib(nibName: "\(RollCell.self)", bundle: Bundle.main),
+                           forCellReuseIdentifier: cellIdentifier)
+    }
+    
     private func setupSearchController() {
         searchController = UISearchController(searchResultsController: nil)
         searchController.searchResultsUpdater = self
@@ -118,9 +122,40 @@ final class RollViewController: UIViewController {
         navigationItem.searchController = searchController
     }
     
+    private func setupKeyboard() {
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(adjustKeyboard),
+                                               name: UIResponder.keyboardWillChangeFrameNotification,
+                                               object: nil)
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(adjustKeyboard),
+                                               name: UIResponder.keyboardWillHideNotification,
+                                               object: nil)
+    }
+    
     private func filterContent(for searchText: String) {
         searchedAcross = across.filter { $0.question.localizedCaseInsensitiveContains(searchText) }
         searchedDown = down.filter { $0.question.localizedCaseInsensitiveContains(searchText) }
+    }
+    
+    @objc
+    private func adjustKeyboard(notification: Notification) {
+        guard
+            let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue
+        else { return }
+
+        let keyboardScreenEndFrame = keyboardValue.cgRectValue
+        let keyboardViewEndFrame = view.convert(keyboardScreenEndFrame, from: view.window)
+
+        if notification.name == UIResponder.keyboardWillHideNotification {
+            tableView.contentInset = .zero
+        } else {
+            let bottom = keyboardViewEndFrame.height - view.safeAreaInsets.bottom
+            tableView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: bottom, right: 0)
+        }
+
+        tableView.scrollIndicatorInsets = tableView.contentInset
     }
 }
 
@@ -146,11 +181,19 @@ extension RollViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView,
                    numberOfRowsInSection section: Int) -> Int {
-        guard !searchController.isActive else {
-            return section == 0 ? searchedAcross.count : searchedDown.count
+        switch (section, searchController.isActive) {
+            case (0, true):
+                return searchedAcross.count
+                
+            case (0, false):
+                return across.count
+
+            case (_, true):
+                return searchedDown.count
+                
+            case (_, false):
+                return down.count
         }
-        
-        return section == 0 ? across.count : down.count
     }
     
     func tableView(_ tableView: UITableView,
@@ -170,13 +213,13 @@ extension RollViewController: UITableViewDataSource {
             word = down[indexPath.row]
         }
         
-        if let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? RollCell {
-            cell.nameLabel?.text = mode == .questions ? word.question : word.answer
-            cell.indexLabel?.text = String((words.enumerated().first(where: { $0.element == word })?.offset ?? 0) + 1)
-            return cell
-        } else {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as? RollCell else {
             return UITableViewCell(style: .default, reuseIdentifier: cellIdentifier)
         }
+        
+        cell.nameLabel?.text = mode == .questions ? word.question : word.answer
+        cell.indexLabel?.text = String((words.enumerated().first(where: { $0.element == word })?.offset ?? 0) + 1)
+        return cell
     }
 }
 
